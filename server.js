6c5a83936d590663bce8a52282f3f3c60b86f7a3
Jessi <jessivelazq1@gmail.com>
2021-04-19 15:15:17 -0700

    changed server.js

--- server.js
+++ server.js
@@ -12,18 +12,27 @@ const stamper = require('./middleware/stamper.js');
 const notFoundHandler = require('./handlers/404.js');
 const errors = require('./handlers/500.js');
 
+app.get('/', stamper, (req, res) => {
+  res.status(200).send('Hello World')
+})
+
 app.get('/test-route', (req, res) => {
   res.json({ msg: 'this worked' });
 });
 
 // route level middleware happens in the middle of the process
 app.get('/data', stamper, (req, res) => {
-  let output = { time: req.timestamp }
-  res.json(output);
-})
+  let outputObject = {
+    10: "even",
+    5: "odd",
+    "time": req.timestamp // we got this from the middleware
+  }
 
-app.get('purposeful-error', (req, res, next) => {
-  next('some words');
+  res.status(200).json(outputObject);
+});
+
+app.get('/bad', (req, res, next) => {
+  next('you messsed up')
 });
 
 // app.use -> global middleware function - all incoming requests will pipe through this
@@ -33,9 +42,7 @@ app.use('*', notFoundHandler);
 app.use(errors);
 
 function start(port) {
-  app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`);
-  });
+  app.listen(port, () => console.log(`Server up on port ${port}`))
 }
 
 module.exports = {
